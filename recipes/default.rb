a3577f4f29324c6dfea9c20179773df3e4bd57a0
Antons Kranga <akranga@gmail.com>
2014-01-08 23:13:46 +0200

    Improved vertx node attributes

--- recipes/default.rb
+++ recipes/default.rb
@@ -3,13 +3,14 @@
 # Recipe:: default
 #
 # Author 2012 Ryley Gahagan (rgahagan@bloomhealthco.com)
+# Author 2014 Antons Kranga (akranga@gmail.com)
 # 
 
 include_recipe "java"
 include_recipe "ark"
 
 ark "vertx" do
-  url node[:vertx][:url] + node[:vertx][:version] + ".final.zip"
+  url node[:vertx][:mirror]
   home_dir node[:vertx][:home]
   version node[:vertx][:version]
   append_env_path true
