a5858bcaf17a264ada3ba0eefc57d68e2ab75201
Brian Scott <brainscott@gmail.com>
2014-06-13 10:03:41 -0700

    Fixed typo

--- attributes/attributes.rb
+++ attributes/attributes.rb
@@ -8,4 +8,4 @@
 node.default[:vertx][:version]    = '2.1'
 node.default[:vertx][:home]       = '/usr/local/vertx'
 node.default[:vertx][:url]        = 'http://dl.bintray.com/vertx/downloads/vert.x-'
-node.default[:vertx][:version] = ".zip"
+node.default[:vertx][:file_ext] = ".zip"
