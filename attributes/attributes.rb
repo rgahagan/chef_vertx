f85e326dece6f399e325df3819c60563dde9359b
Brian Scott <brainscott@gmail.com>
2014-06-13 09:36:18 -0700

    Vertx version bump to latest

--- attributes/attributes.rb
+++ attributes/attributes.rb
@@ -5,6 +5,6 @@
 # Author 2012 Ryley Gahagan (rgahagan@bloomhealthco.com)
 # 
 #
-default[:vertx][:version]    = '1.3.0'
+default[:vertx][:version]    = '2.1'
 default[:vertx][:home]       = '/usr/local/vertx'
 default[:vertx][:url]        = 'http://dl.bintray.com/vertx/downloads/vert.x-'
