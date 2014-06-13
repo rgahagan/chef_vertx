297da878e00bacadf261961c714fdf03f03652f4
Brian Scott <brainscott@gmail.com>
2014-06-13 09:35:53 -0700

    Update metadata.rb

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email "rgahagan@bloomhealthco.com"
 license          "Apache 2.0"
 description      "Installs/Configures vertx"
 long_description "Installs/Configures vertx"
-version          "0.0.1"
+version          "0.0.3"
 
 depends "ark"
 depends "java"
